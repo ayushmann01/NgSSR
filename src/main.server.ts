c98ffbc012ca09fd273dd2c4993c182cc438d1ea
Ayushman Raghav <ayushmanraghav.ra@gmail.com>
2023-10-22 17:21:26 +0530

    completed SSR setup

---
+++ src/main.server.ts
@@ -0,0 +1,13 @@
+export { AppServerModule } from './app/app.module.server';
+import '@angular/platform-server/init';
+
+import { enableProdMode } from '@angular/core';
+
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+// export { AppServerModule } from './app/app.server.module';
+export { renderModule } from '@angular/platform-server';
